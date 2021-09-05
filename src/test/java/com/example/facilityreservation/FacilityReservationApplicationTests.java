278e1ca5620967282e98902a31749541239ea28b
takashi-stack <takashi-stack@users.noreply.github.com>
2021-09-05 17:33:50 +0900

    コンフリクトの修正

--- src/test/java/com/example/facilityreservation/FacilityReservationApplicationTests.java
+++ src/test/java/com/example/facilityreservation/FacilityReservationApplicationTests.java
@@ -1,22 +1,6 @@
 package com.example.facilityreservation;
 
-<<<<<<< HEAD
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class FacilityReservationApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
-=======
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -32,4 +16,3 @@ public class FacilityReservationApplicationTests {
     public void contextLoads() {
     }
 }
->>>>>>> 3459847 (テストコード追加)
